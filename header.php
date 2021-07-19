998bb0ad7edaf73b4f452de29bd7247668310ac8
Pooja <pooja.kr.374@gmail.com>
2021-07-20 03:12:32 +0530

    adding e-commerce

---
+++ header.php
@@ -0,0 +1,20 @@
+        
+
+<nav class="nav navbar navbar-inverse index_back">
+            <div class="container nav_container ">
+                <div class="navbar-header ">
+                       <a href="#" class="navbar-brand">E-Store</a>
+                </div>
+                      
+<!--                <div class="collapse navbar-collapse" id="myNavbar">-->
+                <div>
+                         <ul class="nav navbar-nav navbar-right">
+                         <li><a href="sign_up.html" target="_blank"><span class="glyphicon glyphicon-user"></span> Sign up</a></li>
+                         <li><a href="sign_in.html" target="_blank"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                         <li><a href="#"><span class="glyphicon glyphicon-th-list"></span> About us</a></li>
+                         <li><a href="#"><span class=" glyphicon glyphicon-earphone"></span> Contact us</a></li>
+                         
+                             
+                </div>
+            </div>
+</nav>
